fa3f24b9bc81b0e1fcec5a6502c3c3898f98d7f4
Wasiu Idowu <hoxtygen@live.com>
2020-08-30 07:15:09 +0100

    Add dice image to the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDice = 3;
+  int rightDice = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("Left dice clicked");
+              },
+              child: Image(
+                image: AssetImage("images/dice$leftDice.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                print("Right dice clicked");
+              },
+              child: Image(
+                image: AssetImage('images/dice$rightDice.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
